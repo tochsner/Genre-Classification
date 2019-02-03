f7c12c5097a98083c121566293a4da661f4c5e83
TobiaAlt <tobia.ochsner@hotmail.comgit config --global user.email tobia.ochsner@hotmail.com>
2019-02-03 15:31:16 +0100

    Cleaned

--- logger.py
+++
@@ -1,7 +0,0 @@
-class Logger():
-    def __init__(self, name):
-        self.Name = name
-
-    def log(self, text):
-        with open(self.Name + ".txt", "a+") as logFile:
-            logFile.write(str(text) + "\n")
